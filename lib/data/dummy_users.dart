2bed297745a298a0a8dbc96a45be6b7a88c0a043
Luan <luan.1973468250@gmail.com>
2022-04-24 14:07:36 -0400

    Curso Flutter - Cod3r
    CRUD - Cadastro Usuário

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_crud/models/user.dart';
+
+const dummyUsers = {
+  '1': User(
+    id: '1',
+    name: 'Gabriely',
+    email: 'gabriely@ifro.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2021/10/31/16/53/squid-game-6758082_960_720.png',
+  ),
+  '2': User(
+    id: '2',
+    name: 'Felipe',
+    email: 'felipe@ifro.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png',
+  ),
+  '3': User(
+    id: '3',
+    name: 'Thiana',
+    email: 'thiana@ifro.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png',
+  ),
+  '4': User(
+    id: '4',
+    name: 'Aristoteles',
+    email: 'aristoteles@ifro.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png',
+  )
+};
